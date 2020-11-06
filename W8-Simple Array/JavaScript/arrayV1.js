8193c08a9af3b013db4c477f9343660e52db2b67
Leah Graydon <17013813@uhi.ac.uk>
2020-11-06 13:26:46 +0000

    W8- Array Simple

---
+++ W8-Simple Array/JavaScript/arrayV1.js
@@ -0,0 +1,13 @@
+var myfam = new Array();
+myfam[0] = "Weener ";
+
+var data = new Array();
+data[0] = 'Hello';
+data[1] = 'See ya';
+
+alert (data[0]);
+alert(myfam[0]);
+alert (data[1]);
+
+
+document.write(myfam);
